c6a733bd76553417bcf224e1e4852df69c175231
vitorpinho376 <hi@vitor.mobi>
2020-12-28 13:27:19 -0300

    add prettier

--- js/app.js
+++ js/app.js
@@ -1,15 +1,14 @@
 //Window Listener
 
-    const topBar = document.querySelector('.attached-header');
-    
-    var myScrollFunc = function () {
+      const topBar = document.querySelector('.attached-header');
+        var myScrollFunc = function () {
         var topScroll = window.scrollY;
         if (topScroll >= 600) {
             topBar.classList.remove('hidden');
         } else {
             topBar.classList.add('hidden');
         }
-    };
+    };  
     
     window.addEventListener("scroll", myScrollFunc);
 
